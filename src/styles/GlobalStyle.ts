c7c679f8f5a7395e49c73373f5be76d5be839886
fernando-meira <thomwork@outlook.com>
2021-03-24 22:08:58 -0300

    feat: 🎸 Fundamentals next class completed

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  
+
+  body {
+    color: fff;
+    background: #121214;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
