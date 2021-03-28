12fbe54275dbcd288b19e0791cd0896c5d84bad0
fernando-meira <thomwork@outlook.com>
2021-03-27 23:28:22 -0300

    feat: 🎸 Dinamic static pages

--- src/pages/top10.tsx
+++ src/pages/top10.tsx
@@ -24,13 +24,14 @@ export default function Top10({ products }: ITop10Props) {
   )
 }
 
-export const getStaticProps: GetStaticProps<ITop10Props> = async () => {
+export const getStaticProps: GetStaticProps<ITop10Props> = async (context) => {
   const response = await fetch("http://localhost:3333/products");
   const products = await response.json();
 
   return {
     props: {
       products,
-    }
+    },
+    revalidate: 20,
   }
 }
